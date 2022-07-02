77618bcc51a17da5a6cd7d70a44fdb30373c70fe
Ireg27 <geri.manjani@gmail.com>
2022-07-02 16:27:27 +0200

    Configured tailwind css

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,11 +3,15 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700" rel="stylesheet" />
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
     <script src="{{ mix('/js/app.js') }}" defer></script>
     @inertiaHead
+    <style>
+    </style>
   </head>
   <body>
     @inertia
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   </body>
 </html>
