207074aa5db048c75cb482f2f73026e034a40b0d
Ireg27 <geri.manjani@gmail.com>
2022-07-03 17:45:24 +0200

    Fixed navigation and working on website

--- resources/js/Layouts/Landpage.vue
+++ resources/js/Layouts/Landpage.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <Disclosure as="nav" class="bg-white shadow" v-slot="{ open }">
+        <Disclosure as="nav" class="shadow fixed w-full z-30 top-0 text-white" :class="{ 'bg-slate-800': !view.topOfPage}" v-slot="{ open }">
             <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div class="flex justify-between h-16">
                     <div class="flex">
@@ -12,36 +12,35 @@
                             />
                             <img
                                 class="hidden lg:block h-8 w-auto"
-                                src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg"
+                                src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-900-text.svg"
                                 alt="Workflow"
                             />
                         </div>
                         <div class="hidden sm:ml-6 sm:flex sm:space-x-8">
-                            <!-- Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" -->
                             <Link
                                 :href="route('landpage')"
-                                class="text-gray-900 inline-flex items-center px-1 pt-1 text-sm font-medium"
+                                class="text-white inline-flex items-center px-1 pt-1 text-sm font-medium"
                                 :class="{'border-b-2 border-indigo-500': $page.component === 'Welcome'}"
                             >
                                 Dashboard
                             </Link>
                             <Link
                                 href="#"
-                                class="text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 text-sm font-medium"
+                                class="text-white hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 text-sm font-medium"
                                 :class="{'border-b-2 border-indigo-500': $page.component === 'todo'}"
                             >
                                 Team
                             </Link>
                             <Link
                                 href="#"
-                                class="text-gray-500 hover:text-gray-700 inline-flex items-center px-1 pt-1 text-sm font-medium"
+                                class="text-white hover:text-gray-700 inline-flex items-center px-1 pt-1 text-sm font-medium"
                                 :class="{'border-b-2 border-indigo-500': $page.component === 'todo'}"
                             >
                                 Projects
                             </Link>
                             <Link
                                 :href="route('pages.about')"
-                                class="text-gray-500 hover:text-gray-700 inline-flex items-center px-1 pt-1 text-sm font-medium"
+                                class="text-white hover:text-gray-700 inline-flex items-center px-1 pt-1 text-sm font-medium"
                                 :class="{'border-b-2 border-indigo-500': $page.component === 'About'}"
                             >
                                 About
@@ -49,11 +48,11 @@
                         </div>
                     </div>
                     <div v-if="! $page.props.auth.user" class="hidden sm:ml-6 sm:flex sm:items-center">
-                        <Link :href="route('login')" class="text-sm text-gray-700 underline">
+                        <Link :href="route('login')" class="text-sm text-white underline">
                             Log in
                         </Link>
 
-                        <Link :href="route('register')" class="ml-4 text-sm text-gray-700 underline">
+                        <Link :href="route('register')" class="ml-4 text-sm text-white underline">
                             Register
                         </Link>
                     </div>
@@ -90,67 +89,32 @@
                     <DisclosureButton
                         as="a"
                         href="#"
-                        class="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
+                        class="border-transparent text-white hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
                         >Team</DisclosureButton
                     >
                     <DisclosureButton
                         as="a"
                         href="#"
-                        class="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
+                        class="border-transparent text-white hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
                         >Projects</DisclosureButton
                     >
                     <DisclosureButton
                         as="a"
                         href="#"
-                        class="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
+                        class="border-transparent text-white hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
                         >Calendar</DisclosureButton
                     >
                 </div>
                 <div class="pt-4 pb-3 border-t border-gray-200">
-                    <div class="flex items-center px-4">
-                        <div class="flex-shrink-0">
-                            <img
-                                class="h-10 w-10 rounded-full"
-                                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                alt=""
-                            />
-                        </div>
-                        <div class="ml-3">
-                            <div class="text-base font-medium text-gray-800">
-                                Tom Cook
-                            </div>
-                            <div class="text-sm font-medium text-gray-500">
-                                tom@example.com
-                            </div>
-                        </div>
-                        <button
-                            type="button"
-                            class="ml-auto flex-shrink-0 bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                        >
-                            <span class="sr-only">View notifications</span>
-                            <BellIcon class="h-6 w-6" aria-hidden="true" />
-                        </button>
-                    </div>
-                    <div class="mt-3 space-y-1">
-                        <DisclosureButton
-                            as="a"
-                            href="#"
-                            class="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100"
-                            >Your Profile</DisclosureButton
-                        >
-                        <DisclosureButton
-                            as="a"
-                            href="#"
-                            class="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100"
-                            >Settings</DisclosureButton
-                        >
-                        <DisclosureButton
-                            as="a"
-                            href="#"
-                            class="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100"
-                            >Sign out</DisclosureButton
-                        >
-                    </div>
+                    <Link :href="route('login')" class="border-transparent text-white hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium">
+                        Log in
+                    </Link>
+                    <Link :href="route('register')" class="border-transparent text-white hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium">
+                        Register
+                    </Link>
+                    <Link v-if="$page.props.auth.user" :href="route('dashboard')" class="text-sm text-gray-700 underline">
+                        Dashboard
+                    </Link>
                 </div>
             </DisclosurePanel>
         </Disclosure>
@@ -185,5 +149,24 @@ export default {
         XIcon,
         Link,
     },
+    data () {
+        return {
+            view: {
+                topOfPage: true
+            }
+        }
+    },
+    beforeMount() {
+        window.addEventListener('scroll', this.handleScroll)
+    },
+    methods: {
+    handleScroll(){
+      if(window.pageYOffset>0){
+        if(this.view.topOfPage) this.view.topOfPage = false
+      } else {
+        if(!this.view.topOfPage) this.view.topOfPage = true
+      }
+    }
+  },
 };
 </script>
