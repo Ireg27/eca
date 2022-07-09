a92a47d9dae949c742e31e80dce338e26083a422
Ireg27 <geri.manjani@gmail.com>
2022-07-09 20:27:43 +0200

    Team Page Done

---
+++ resources/js/Pages/Commercial/Team/Header.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="relative overflow-hidden">
+      <main>
+        <div class="pt-20 bg-slate-800 sm:pt-20 lg:pt-[100px] lg:pb-14 lg:overflow-hidden">
+          <div class="mx-auto max-w-7xl lg:px-8">
+            <div class="lg:grid lg:grid-cols-2 lg:gap-8">
+              <div class="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:px-0 lg:text-left lg:flex lg:items-center">
+                <div class="lg:py-24">
+                  <a href="#" class="inline-flex items-center text-white bg-black rounded-full p-1 pr-2 sm:text-base lg:text-sm xl:text-base hover:text-gray-200">
+                    <span class="px-3 py-0.5 text-white text-xs font-semibold leading-5 uppercase tracking-wide bg-indigo-500 rounded-full">We're hiring</span>
+                    <span class="ml-4 text-sm">Visit our careers page</span>
+                    <ChevronRightIcon class="ml-2 w-5 h-5 text-gray-500" aria-hidden="true" />
+                  </a>
+                  <h1 class="mt-4 text-4xl tracking-tight font-extrabold text-white sm:mt-5 sm:text-6xl lg:mt-6 xl:text-6xl">
+                    <span class="block">A better way to</span>
+                    <span class="block text-indigo-400">ship web apps</span>
+                  </h1>
+                  <p class="mt-3 text-base text-gray-300 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui Lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat.</p>
+
+                </div>
+              </div>
+              <div class="mt-12 -mb-16 sm:-mb-48 lg:m-0 lg:relative">
+                <div class="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:max-w-none lg:px-0">
+                  <!-- Illustration taken from Lucid Illustrations: https://lucid.pixsellz.io/ -->
+                  <img class="w-full lg:absolute lg:inset-y-0 lg:left-0 lg:h-full lg:w-auto lg:max-w-none" src="https://tailwindui.com/img/component-images/cloud-illustration-indigo-400.svg" alt="img" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+</template>
